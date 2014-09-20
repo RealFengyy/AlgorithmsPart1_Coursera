b9d6e8bf207fcee5bab24bc5c7e821348839132d
fyiyang91 <fyiyang91@gmail.com>
2014-09-20 09:24:04 -0700

    Update Brute.java

--- ConlinearPoints/Brute.java
+++ ConlinearPoints/Brute.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 public class Brute {
 	
 	public static void main (String[] args) throws FileNotFoundException{
-
 		/* for debug in eclipse */		
 		if (null != args) {
 			Scanner in = new Scanner(new File(args[0]));
@@ -21,13 +20,10 @@ public class Brute {
 				i++;
 			}
 			assert (i == N);
-	
 			Arrays.sort(pointArr);
-            
 			// set window
 			StdDraw.setXscale(0, 32768);
 			StdDraw.setYscale(0, 32768);
-			
 			for (int i1 = 0; i1 < pointArr.length; i1++) {
 				Point p = pointArr[i1];
 				p.draw();
@@ -40,7 +36,8 @@ public class Brute {
 								Point s = pointArr[l];
 								if (p.slopeTo(q) == q.slopeTo(r) &&
 									q.slopeTo(r) == r.slopeTo(s)) {
-									StdOut.println(p + " -> " + q + " -> " + r + " -> " + s);
+									StdOut.println(p + " -> " + q + " -> " + 
+									r + " -> " + s);
 									p.drawTo(s);
 								}
 							}
