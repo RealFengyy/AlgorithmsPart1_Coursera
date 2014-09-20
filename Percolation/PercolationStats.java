e90a4e7c3afa2ccbf670cd5ab966d9db266cb468
fyiyang91 <fyiyang91@gmail.com>
2014-09-20 09:15:51 -0700

    Update PercolationStats.java

--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -3,16 +3,14 @@
  * class for percolation statistic computation
  */
 public class PercolationStats {
-	
-	
 	// instance variables
 	private int N; // grid size
 	private int T; // number of tests to perform
 	private double[] thData; // holds threhold data
 		
-	/*
-	 * constructor
-	 */
+  /*
+   * constructor
+   */
    public PercolationStats(int N, int T) {
 	   // perform T independent computational experiments on an N-by-N grid
 	   if(N <= 0 || T <= 0) {
@@ -22,21 +20,21 @@ public class PercolationStats {
 	   this.T = T;
 	   this.N = N;
 	   thData = new double[T];
-	   
 	   mcSimulation();
    }
+   
    
    /*
     * get the average threshold
     */
    public double mean() {
-	   
 	   double sum = 0.0;
 	   for(int i = 0; i < thData.length; i++) {
 		   sum += thData[i];	   
 	   }
 	   return sum/thData.length;
    }
+   
    
    /*
     * get the standard deviation
@@ -46,9 +44,9 @@ public class PercolationStats {
 	   double mean = this.mean();
 	   double sum = 0.0;
 	   for(int i = 0; i< thData.length; i++) sum += (thData[i] - mean) * (thData[i] - mean);
-	   
 	   return Math.sqrt(sum / (thData.length - 1));
    }
+   
    
    /*
     * get the lower bound of the 95% confidence interval
@@ -61,6 +59,7 @@ public class PercolationStats {
 	   //return cLow;
    }
    
+   
    /*
     * get the upper bound of the 95% confidence intervel
     */
@@ -72,20 +71,15 @@ public class PercolationStats {
    }
    
    
-   
    private void mcSimulation() {
-	   
 	   Percolation testPDriver;
 	   // T simulations
 	   for(int i = 0; i < T; i++) {
 		   testPDriver = new Percolation(N);
 		   int openSite = 0;
- 
 		   while(!(openSite == N * N || testPDriver.percolates())) {
-			   
 			   int row = StdRandom.uniform(1, N + 1);
 			   int col = StdRandom.uniform(1, N + 1);
-			   
 			   if(!testPDriver.isOpen(row, col)) {
 				   testPDriver.open(row, col);
 				   openSite++;
@@ -97,6 +91,7 @@ public class PercolationStats {
 	   }
    }
    
+   
    /*
     * test driver, parse command line argument and print the ouput to the standard ouput
     */
@@ -105,19 +100,15 @@ public class PercolationStats {
 	   int gridSize = Integer.parseInt(args[0]);
 	   int testNum = Integer.parseInt(args[1]);
 	   PercolationStats pStat = new PercolationStats(gridSize, testNum);
-	   
 	   pStat.mcSimulation();
 	   double mean = pStat.mean();
 	   double sd = pStat.stddev();
 	   double cLow = pStat.confidenceLo();
 	   double cHigh = pStat.confidenceHi();
-
-	   
 	   StdOut.println("mean                    = " + mean);
 	   StdOut.println("stddev                  = " + sd);
 	   StdOut.println("95% confidence interval = " + cLow + ", " + cHigh);
-	   
-	   
-	   
    }
+   
+   
 }
